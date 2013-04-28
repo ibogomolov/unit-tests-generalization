a8867b7464eca27e284684c24674fc71d5a402aa
Igor Bogomolov <igor.bogomolov@gmail.com>
2013-04-28 23:17:50 +0200

    After the meeting.

--- buttonYui.js
+++ buttonYui.js
@@ -4,27 +4,27 @@ var testCase = new YUITest.TestCase({
     other: /type hi/,
     hi: /hello/,
     
-	setUp : function () {
-        document.getElementById('in').value = "";
-        document.getElementById('div').innerHTML = "";
-    },
+	// setUp : function () {
+ //        document.getElementById('in').value = "";
+ //        document.getElementById('div').innerHTML = "";
+ //    },
 
-    tearDown : function () {
-        this.setUp();
-    }, 
-
-    testWrongInput : function () {
-        document.getElementById('in').value = "abc";
-        cl();
-        YUITest.Assert.isTrue(this.other.test(document.getElementById('div').innerHTML));
-        YUITest.Assert.isFalse(this.hi.test(document.getElementById('div').innerHTML));
-    },
+ //    tearDown : function () {
+ //        this.setUp();
+ //    }, 
 
     testCorrectInput : function () {
-        document.getElementById('in').value = "hi";
-        cl();
+        // document.getElementById('in').value = "hi";
+        cl(); // function under test
         YUITest.Assert.isFalse(this.other.test(document.getElementById('div').innerHTML));
         YUITest.Assert.isTrue(this.hi.test(document.getElementById('div').innerHTML));
     }
+
+    // testWrongInput : function () {
+    //     // document.getElementById('in').value = "abc";
+    //     cl();
+    //     YUITest.Assert.isTrue(this.other.test(document.getElementById('div').innerHTML));
+    //     YUITest.Assert.isFalse(this.hi.test(document.getElementById('div').innerHTML));
+    // }
 
 });
